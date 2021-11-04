7d37c594e82da7d36833eafdb378a86f2e95ae0a
brunaopdejesus <bruna.oliveira.656667@gmail.com>
2021-11-04 11:25:00 -0300

    04-11

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+
+    $serverName = 'localhost';
+    $userName = 'root';
+    $password = 'bcd127';
+    $dataBase = 'cadastro';
+
+    $conn = new mysqli($serverName, $userName, $password, $dataBase);
+
+    if ($conn->connect_error) {
+        
+        die("Connection error: " . $conn->connect_error);
+
+    }
+
+    return $conn;
+
+?>
